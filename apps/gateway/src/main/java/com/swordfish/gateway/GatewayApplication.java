794fc09e0b3f7c2f649b608a7e41f175f574bd9e
anhluong660 <anhluong660@gmail.com>
2024-08-30 14:44:30 +0700

    config log4j spring boot

--- apps/gateway/src/main/java/com/swordfish/gateway/GatewayApplication.java
+++ apps/gateway/src/main/java/com/swordfish/gateway/GatewayApplication.java
@@ -1,16 +1,20 @@
 package com.swordfish.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(GatewayApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
 
-        System.out.println("+++++++++++++++++++++++++++++++++++++++");
-        System.out.println("[[ Start Gateway Application Success ]]");
-        System.out.println("+++++++++++++++++++++++++++++++++++++++");
+        log.info("+++++++++++++++++++++++++++++++++++++++");
+        log.info("[[ Start Gateway Application Success ]]");
+        log.info("+++++++++++++++++++++++++++++++++++++++");
     }
 }
--- apps/user-manager/src/main/java/com/swordfish/users/UserManagerApplication.java
+++ apps/user-manager/src/main/java/com/swordfish/users/UserManagerApplication.java
@@ -1,16 +1,20 @@
 package com.swordfish.users;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserManagerApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(UserManagerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(UserManagerApplication.class, args);
 
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++++");
-        System.out.println("[[ Start User Manager Application Success ]]");
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++++");
+        log.info("++++++++++++++++++++++++++++++++++++++++++++");
+        log.info("[[ Start User Manager Application Success ]]");
+        log.info("++++++++++++++++++++++++++++++++++++++++++++");
     }
 }
