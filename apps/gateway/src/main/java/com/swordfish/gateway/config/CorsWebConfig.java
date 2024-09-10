0fcf76a88ead7dc550c8f61e8595ae7ca7fec1d9
anhluong660 <anhluong660@gmail.com>
2024-09-10 14:19:01 +0700

    config Cors

---
+++ apps/gateway/src/main/java/com/swordfish/gateway/config/CorsWebConfig.java
@@ -0,0 +1,26 @@
+package com.swordfish.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+@Configuration
+public class CorsWebConfig {
+
+    @Bean
+    public CorsWebFilter filter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setMaxAge(86400L);
+        config.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
